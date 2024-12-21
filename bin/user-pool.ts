bfad1971afed70849ca16ce6b46f94e8eaedcc10
jj <jj@ondemandenv.dev>
2024-12-21 18:21:26 -0500

    init

---
+++ bin/user-pool.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import {StackProps} from "aws-cdk-lib";
+import {OndemandContractsSandbox} from "@ondemandenv/odmd-contracts-sandbox";
+import {OdmdEnverCdk} from "@ondemandenv/contracts-lib-base";
+import {UserPoolStack} from "../lib/user-pool-stack";
+
+const app = new cdk.App();
+
+
+async function main() {
+
+    const buildRegion = process.env.CDK_DEFAULT_REGION;
+    const buildAccount = process.env.CDK_DEFAULT_ACCOUNT;
+    if (!buildRegion || !buildAccount) {
+        throw new Error("buildRegion>" + buildRegion + "; buildAccount>" + buildAccount)
+    }
+
+    const props = {
+        env: {
+            account: buildAccount,
+            region: buildRegion
+        }
+    } as StackProps;
+
+    new OndemandContractsSandbox(app)
+
+    const targetEnver = OndemandContractsSandbox.inst.getTargetEnver() as OdmdEnverCdk
+
+    new UserPoolStack(app, targetEnver.getRevStackNames()[0], props)
+
+}
+
+
+console.log("main begin.")
+main().catch(e => {
+    console.error(e)
+    throw e
+}).finally(() => {
+    console.log("main end.")
+})
---
+++ lib/user-pool-stack.ts
@@ -0,0 +1,120 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {
+    OAuthScope, ProviderAttribute,
+    UserPool,
+    UserPoolClient,
+    UserPoolClientIdentityProvider,
+    UserPoolIdentityProviderGoogle
+} from "aws-cdk-lib/aws-cognito";
+import {SecretValue} from "aws-cdk-lib";
+import {HostedZone} from "aws-cdk-lib/aws-route53";
+import {Certificate, CertificateValidation} from "aws-cdk-lib/aws-certificatemanager";
+import {OndemandContractsSandbox} from "@ondemandenv/odmd-contracts-sandbox";
+import {
+    CognitoUserPoolEnver
+} from "@ondemandenv/odmd-contracts-sandbox/lib/repos/user-pool/CognitoUserPoolCdkOdmdBuild";
+import {OdmdCrossRefProducer, OdmdEnverCdk, OdmdShareOut} from "@ondemandenv/contracts-lib-base";
+
+export class UserPoolStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const myEnver = OndemandContractsSandbox.inst.getTargetEnver() as CognitoUserPoolEnver
+
+        const userPool = new UserPool(this, 'Pool', {
+            userPoolName: 'auth.ondemandenv.link',
+            selfSignUpEnabled: true,
+            signInAliases: {
+                email: true,
+            },
+            standardAttributes: {
+                email: {
+                    required: true,
+                    mutable: true,
+                },
+            }
+        });
+
+        const oauthUserpoolClient = new UserPoolClient(this, 'UserPoolClient', {
+            userPool,
+            oAuth: {
+                flows: {
+                    authorizationCodeGrant: true,
+                },
+                scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.PROFILE],
+                callbackUrls: [
+                    // 'http://localhost:5173/callback',
+                    ...myEnver.llmChatCallbackUrl.map(c => c.getSharedValue(this))
+                ],
+                logoutUrls: [
+                    ...myEnver.llmChatLogoutUrl.map(c => c.getSharedValue(this))
+                ],
+
+            },
+            supportedIdentityProviders: [
+                UserPoolClientIdentityProvider.GOOGLE,
+            ],
+        });
+
+        oauthUserpoolClient.node.addDependency(new UserPoolIdentityProviderGoogle(this, 'GoogleProvider', {
+            userPool,
+            clientId: '425156547044-a8gnro92fhtf6shstfrune7oc8i6i0hf.apps.googleusercontent.com',
+            clientSecretValue: SecretValue.ssmSecure('/google_oauth_client_secret'),
+            scopes: ['email', 'profile', 'openid'],
+
+            attributeMapping: {
+                email: ProviderAttribute.GOOGLE_EMAIL,
+                emailVerified: ProviderAttribute.GOOGLE_EMAIL_VERIFIED,
+                givenName: ProviderAttribute.GOOGLE_GIVEN_NAME,
+                familyName: ProviderAttribute.GOOGLE_FAMILY_NAME,
+            }
+        }));
+
+        const hostedZoneId = 'Z07732022HSGPH3GRGCVY'
+        const zoneName = 'auth.ondemandenv.link'
+
+        const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+            hostedZoneId,
+            zoneName,
+        });
+
+        const domainName: string = zoneName
+
+        const domain = userPool.addDomain('AuthDomain', {
+            cognitoDomain: {
+                domainPrefix: 'odmd-sandbox',
+            },
+            customDomain: {
+                domainName,
+                certificate: new Certificate(this, 'Certificate', {
+                    domainName,
+                    validation: CertificateValidation.fromDns(hostedZone)
+                }),
+            }
+        });
+
+        const resourceClient = new UserPoolClient(this, 'ResourceClient', {
+            userPool: userPool,
+            generateSecret: false,
+            authFlows: {
+                userSrp: true,
+                userPassword: true,
+            },
+            preventUserExistenceErrors: true,
+        });
+
+        new OdmdShareOut(this, new Map<OdmdCrossRefProducer<CognitoUserPoolEnver>, any>([
+            [myEnver.userPoolId, userPool.userPoolId],
+            [myEnver.userPoolArn, userPool.userPoolArn],
+            [myEnver.oauthUserPoolClientId, oauthUserpoolClient.userPoolClientId],
+            [myEnver.oauth2RedirectUri, domain.baseUrl() + '/oauth2/idpresponse'],
+            [myEnver.resourceUserPoolClientId, resourceClient.userPoolClientId],
+        ]))
+
+        new cdk.CfnOutput(this, 'UserPoolId', {value: userPool.userPoolId});
+        new cdk.CfnOutput(this, 'UserPoolClientId', {value: oauthUserpoolClient.userPoolClientId});
+        new cdk.CfnOutput(this, 'UserPoolDomainName', {value: domain.domainName});
+
+    }
+}
